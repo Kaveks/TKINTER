159e3e1abee5003c4fdc55fce0d1ff34be35b753
Kaveks <pkaveks2@gmail.com>
2022-09-30 08:19:02 +0300

    creating Entry

---
+++ Entry.py
@@ -0,0 +1,11 @@
+# create an entry on the window with a button to execute a command
+from tkinter import *
+
+
+root =Tk()
+
+entry1 = Entry(root,width=50,borderwidth=10)#create entry
+entry1.pack()#display entry
+
+
+root.mainloop()
