cafb3661d5637ac08e086f0bb2bf329b830eccc1
Kaveks <pkaveks2@gmail.com>
2022-09-30 08:58:55 +0300

    add a function to execute a button command up on clicking

--- Entry.py
+++ Entry.py
@@ -8,6 +8,14 @@ entry1 = Entry(root,width=50,borderwidth=10)#create entry
 entry1.pack()#display entry
 
 
-button_entry1=Button(root,text = "run",padx=5,pady=2,fg ="red",bg="blue", )# create a button
+# create a function to execute a command on a button click
+def entryfunc():
+    name =entry1.get()# get whatever entry in entry1 variable
+    greetings = f"hello!  {name}"# concatenate using string formatter
+    label1 = Label(root ,text=greetings)# create label to display entry content up on clicks
+    label1.pack()# display label
+    return
+
+button_entry1=Button(root,text = "run",padx=5,pady=2,fg ="red",bg="blue",command=entryfunc )# create a button
 button_entry1.pack()#display entry
 root.mainloop()
